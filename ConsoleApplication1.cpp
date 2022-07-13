2ac6d9210d56d56eab4d6c73a77f3cc1819301c8
Ksenia <91599073+molodoe-zlo@users.noreply.github.com>
2022-07-14 04:16:31 +0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <algorithm>
+#include "checkers.h"
+using namespace std;
+
+int main()
+{
+    checkers a;
+    a.print();
+    char letterpos; int numpos; char letterpos1; int numpos1;
+    while (true)
+    {
+            cin >> letterpos >> numpos;
+            cin >> letterpos1 >> numpos1;
+            a.play(letterpos, numpos, letterpos1, numpos1);
+            a.print();
+    }
+}
